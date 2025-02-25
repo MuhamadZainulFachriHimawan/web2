66558b2044eca45f0c6ced5f0a23eba5fb7f3289
MuhamadZainulFachriHimawan <muhamadzainulfachri@gmail.com>
2025-02-25 13:10:10 +0700

    menambahkan file

---
+++ variable_user.php
@@ -0,0 +1,11 @@
+<?php
+// mendevinisikan variable
+$nama = 'Huda';
+$umur = 19;
+$berat = 60.60;
+
+echo 'Nama Saya' . $nama; 
+echo '<br/>Umur : '. $umur . 'Tahun';
+echo '<br/>Berat :' . $berat;
+
+?>
