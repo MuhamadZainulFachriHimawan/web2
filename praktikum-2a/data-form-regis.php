16b64d8b360eae2b1ba15679220a82bb64ff5ea9
MuhamadZainulFachriHimawan <muhamadzainulfachri@gmail.com>
2025-03-04 11:14:48 +0700

    Add files via upload

---
+++ praktikum-2a/data-form-regis.php
@@ -0,0 +1,18 @@
+<?php
+$ar_prodi = [
+    'SI' => 'Sistem Informasi',
+    'TI' => 'Teknik Informatika',
+    'BD' => 'Bisnis Digital'
+];
+
+$ar_skill = [
+    'HTML' => 10,
+    'CSS' => 10,
+    'JAVA SCRIPT' => 10,
+    'PHP' => 30,
+    'RWD BOOSTRAP' => 20,
+    'PYTHON' => 30,
+    'JAVA' => 50
+];
+
+$ar_domisili = ["Jakarta","Depok","Bogor","Tangerang","Bekasi","Lainnya"];
